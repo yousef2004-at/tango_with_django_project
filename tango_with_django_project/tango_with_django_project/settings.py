d938ca84eb8167648f71f1e2548df8277c1d2856
yousef2004-at <158359274+yousef2004-at@users.noreply.github.com>
2024-02-08 19:25:22 +0000

    Update settings.py

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -123,7 +123,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+{% autoescape on %}
+    {{ body }}
+{% endautoescape %}
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
