135cbf56d180ab320bc83cfe070b437e481a5dc7
yousef2004-at <158359274+yousef2004-at@users.noreply.github.com>
2024-02-08 16:22:50 +0000

    Update urls.py

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from rango import
+from rango import views
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
