d56f61868f5961c2f5db4440f7b6ae9c8858ea49
yousef2004-at <158359274+yousef2004-at@users.noreply.github.com>
2024-02-09 10:07:18 +0000

    Update settings.py

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -133,3 +133,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+class Category(models.Model):
+name = models.CharField(max_length=128, unique=True)
+def __str__(self): return self.name
+class Page(models.Model):
+category = models.ForeignKey(Category, on_delete=models.CASCADE) title = models.CharField(max_length=128)
+url = models.URLField()
+views = models.IntegerField(default=0)
+def __str__(self): return self.title
